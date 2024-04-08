57b37aeeecf384236786b1142930b6880654ebd2
Dibyas98 <dibyashaktip98.com>
2024-04-08 23:47:09 +0530

    sign in api create

--- control/user.js
+++ control/user.js
@@ -1,22 +1,51 @@
 import userModel from "../model/user.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
+import { errorHandeler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
 const signup = async (req, res, next) => {
-    try {
-        const { username, email, password, mobile } = req.body;
-        const hashedPassword = bcryptjs.hashSync(password, 10);
-        const newUser = new userModel({ username, email, password: hashedPassword,mobile });
-        await newUser.save();
-        res.json({
-            success: true
-        });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const { username, email, password, mobile } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new userModel({
+      username,
+      email,
+      password: hashedPassword,
+      mobile,
+    });
+    await newUser.save();
+    res.json({
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
+const signin = async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const { email, password } = req.body;
+     const validUser = await userModel.findOne({ email });
+    if (!validUser) return next(errorHandeler(404, "User not Found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandeler(401, "Invalid Password"));
+    const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+    res.cookie('access_token',token,{httpOnly:true})
+    const {password:pass,...rest} = validUser._doc
+   
+    res.json({
+        success:true,
+        message:rest
+    });
+  } catch (error) {
+    // next(error);
+    console.log(error);
+  }
+};
 const userControl = {
-    signup
+  signup,
+  signin,
 };
 
 export default userControl;
--- route/user.js
+++ route/user.js
@@ -3,7 +3,8 @@ import userControl from '../control/user.js';
 
 const route = express.Router();
 
-route.post('/signup',userControl.signup)
+route.post('/signup',userControl.signup);
+route.get('/signin',userControl.signin);
 
 
 
