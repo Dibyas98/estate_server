31524f7904247c14e80c89ca18c7d42bd94ad909
Dibyas98 <dibyashaktip98@gmail.com>
2024-06-27 10:39:41 +0530

    cors

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ const app = express();
 dbConnect(process.env.DATABASE_URL)
 
 
-app.use(cors())
+app.use(cors(
+    {
+        origin:'http://localhost:5173/',
+        credentials: true
+    }
+))
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/user',userRouter);
